4e62016063ac5178c92dddb52f5c7f57596eb9ae
windisch <windisch@ovgu.de>
2015-05-21 19:33:50 +0200

    HemmeckeMatrix corrected

--- R/HemmeckeMatrix.R
+++ R/HemmeckeMatrix.R
@@ -6,5 +6,5 @@ O=matrix(rep(0,n*n),n,n);
 v=matrix(rep(1,n),1,n);
 w=matrix(rep(0,n),1,n);
 
-rbind(t(rbind(I,I,O,O,v,w)),t(rbind(O,O,I,I,w,v)),t(rbind(t(w),t(w),t(w),t(w),1,-1)));
+rbind(t(rbind(I,I,O,O,-v,w)),t(rbind(O,O,I,I,w,-v)),t(rbind(t(w),t(w),t(w),t(w),1,1)));
 }
