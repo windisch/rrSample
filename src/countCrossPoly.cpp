e0435fdd483275a7e71ce1d8aa329e1fab5e759c
windisch <windisch@ovgu.de>
2015-05-21 09:46:25 +0200

    comment removed

--- src/countCrossPoly.cpp
+++ src/countCrossPoly.cpp
@@ -13,13 +13,6 @@ double countCrossPoly(int dim,int r){
    double count=0;
    double coeff=0;
    
-   //check whether numbers get too large
-   // if(double(r+dim) > boost::math::max_factorial<double>::value){
-   //   std::cout << "numeric overflow will happen" <<  std::endl;
-   //   throw;
-   //   return 0;
-   //  }
-
    #pragma omp parallel for private(coeff)
    for(int k=0; k<=std::min(r,dim);k++){
        coeff=boost::math::binomial_coefficient<double>(double(dim),double(k))*
