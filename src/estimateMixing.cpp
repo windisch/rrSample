86f59901b6562a809a2e85466dcca4c941457f26
windisch <windisch@ovgu.de>
2015-05-26 18:43:41 +0200

    gcd removed

--- src/estimateMixing.cpp
+++ src/estimateMixing.cpp
@@ -2,13 +2,11 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 #include <omp.h>
 // [[Rcpp::plugins(openmp)]]
-#include <boost/math/common_factor.hpp>
-// [[Rcpp::depends(BH)]]
 
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-double estimateMixing(arma::uvec u,arma::mat constMat,arma::mat moves,int diam){
+double estimateMixing(arma::uvec u,arma::mat constMat,arma::mat moves,int diam,double tol=0.25){
 //estimateMixing computes an upper bound on the mixing time
 //diam should be an upper bound on diameter
 //
@@ -21,6 +19,7 @@ double estimateMixing(arma::uvec u,arma::mat constMat,arma::mat moves,int diam){
   Function countCrossPoly("countCrossPoly");
   //double mixing;
   double nAdaptedMoves;
+  double nIntPoints;
   int gcd;
   arma::ivec rhs(constMat.n_rows);
 
@@ -52,35 +51,20 @@ double estimateMixing(arma::uvec u,arma::mat constMat,arma::mat moves,int diam){
       }
    }
 
-   for (k=0; k<rhs.n_elem; k++){
-      std::cout << rhs[k] << std::endl;
-   }
+   //estimate integer points in (constMat,rhs))
+
+
    
-   //get greatest common divisor of entries of rhs
-   gcd=rhs[0];
-   for (unsigned int i=1; i<rhs.n_elem; i++){
-       gcd=boost::math::gcd(gcd,rhs[i]);
-   }
-      std::cout << "GCD:"  << gcd << std::endl;
-   //right hand side to consider
-   #pragma omp parallel for
-   for (k=0; k<rhs.n_elem; k++){
-      rhs[k]=rhs[k]/gcd;
-   }
 
-   std::cout << "NEW RHS" << std::endl;
-   for (k=0; k<rhs.n_elem; k++){
-      std::cout << rhs[k] << std::endl;
-   }
-
-   //try to estimate the integer points in (constMat,rhs)
    //this is a lower bound on int points in (constMat,gcd*rhs)
-
+   
+   if(nIntPoints==0){
+      nIntPoints=10000; 
+    }
 
 
   //
   //
-  //nIntPoints=1; //Improve this by using count and --ehrhart-taylor!
 
   //mixing=nAdaptedMoves/(4*1);
 
