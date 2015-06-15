990ff75e875a6e71afd6df6bd210910b36cff3f2
windisch <windisch@ovgu.de>
2015-06-15 15:03:47 +0200

    add documentation for countCrossPoly

--- src/countCrossPoly.cpp
+++ src/countCrossPoly.cpp
@@ -7,6 +7,30 @@
 
 using namespace Rcpp;
 
+//' @title Count integer points in the cross polytope
+//'
+//' @description This method counts the integer points in 
+//' the \eqn{r}th dilitation
+//' of the \eqn{n}th dimensional cross-polytope. 
+//' \deqn{ \{v\in Z^n: \sum_{i=1}^n|v_i|\le r \}}
+//' This is done by evaluation the following formula 
+//' \deqn{\sum_{k=0}^n\binom{n}{k}\cdot\binom{r-k+n}{n}}
+//' which is exactly the cardinality of the set given above.
+//'
+//' @param dim integer
+//' @param r integer 
+//' @return the number of integer points in the cross polytope defined by
+//' constMat and rhs. The number is returned in a character string.
+//' @name countCrossPoly
+//' @references
+//' \url{https://en.wikipedia.org/wiki/Cross-polytope}
+//' @seealso
+//' \code{\link{countIntPoints}}
+//' \code{\link{countFiber}}
+//' @usage
+//' A<-matrix(c(1,1,1,1,1,0),2,3)
+//' b<-c("14","20")
+//' countIntPoints(A,b)
 // [[Rcpp::export]]
 double countCrossPoly(int dim,int r){
 
